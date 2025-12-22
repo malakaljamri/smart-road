cc8ca2575d1d7c8c58dabfc2cb9d736a9a60badd
moali <mohammedali01071@gmail.com>
2025-12-22 21:52:00 +0300

    draw intersection on canvas

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@ use crate::input::InputHandler;
 
 use render::Sdl2Manager;
 use sdl2::{
-    image::{InitFlag, LoadTexture},
-    rect::Rect,
+    image::{InitFlag, LoadTexture}, pixels::Color, rect::{Point, Rect}
 };
 
 fn main() {
     // Try to create the SDL2 manager (window size 800x600)
-    let mut sdl2_manager = match Sdl2Manager::new("Smart Road", 800, 600) {
+    let mut sdl2_manager = match Sdl2Manager::new("Smart Road", 800, 800) {
         Ok(manager) => manager,
         Err(e) => {
             eprintln!("Failed to initialize SDL2: {}", e);
@@ -60,6 +59,52 @@ fn main() {
             .set_draw_color(sdl2::pixels::Color::RGB(255, 235, 59));
         sdl2_manager.canvas.clear();
 
+        // corner rects
+        sdl2_manager.canvas.set_draw_color(Color::RGB(0, 0, 0));
+        let top_left = Rect::new(0, 0, 295, 295);
+        let top_right = Rect::new(505, 0, 295, 295);
+        let bottom_left = Rect::new(0, 505, 295, 295);
+        let bottom_right = Rect::new(505, 505, 295, 295);
+
+        // draw corner rects
+        sdl2_manager.canvas.fill_rect(top_left).unwrap();
+        sdl2_manager.canvas.fill_rect(top_right).unwrap();
+        sdl2_manager.canvas.fill_rect(bottom_left).unwrap();
+        sdl2_manager.canvas.fill_rect(bottom_right).unwrap();
+
+        // draw lanes
+        // top
+        sdl2_manager.canvas.draw_line(Point::new(330, 0), Point::new(330, 295));
+        sdl2_manager.canvas.draw_line(Point::new(365, 0), Point::new(365, 295));
+        sdl2_manager.canvas.draw_line(Point::new(400, 0), Point::new(400, 295));
+        sdl2_manager.canvas.draw_line(Point::new(435, 0), Point::new(435, 295));
+        sdl2_manager.canvas.draw_line(Point::new(470, 0), Point::new(470, 295));
+        sdl2_manager.canvas.draw_line(Point::new(295, 295), Point::new(400, 295)); // stop
+
+        // bottom
+        sdl2_manager.canvas.draw_line(Point::new(330, 505), Point::new(330, 800));
+        sdl2_manager.canvas.draw_line(Point::new(365, 505), Point::new(365, 800));
+        sdl2_manager.canvas.draw_line(Point::new(400, 505), Point::new(400, 800));
+        sdl2_manager.canvas.draw_line(Point::new(435, 505), Point::new(435, 800));
+        sdl2_manager.canvas.draw_line(Point::new(470, 505), Point::new(470, 800));
+        sdl2_manager.canvas.draw_line(Point::new(400, 505), Point::new(505, 505)); // stop
+
+        // left
+        sdl2_manager.canvas.draw_line(Point::new(0, 330), Point::new(295, 330));
+        sdl2_manager.canvas.draw_line(Point::new(0, 365), Point::new(295, 365));
+        sdl2_manager.canvas.draw_line(Point::new(0, 400), Point::new(295, 400));
+        sdl2_manager.canvas.draw_line(Point::new(0, 435), Point::new(295, 435));
+        sdl2_manager.canvas.draw_line(Point::new(0, 470), Point::new(295, 470));
+        sdl2_manager.canvas.draw_line(Point::new(295, 400), Point::new(295, 505)); // stop
+
+        // right
+        sdl2_manager.canvas.draw_line(Point::new(505, 330), Point::new(800, 330));
+        sdl2_manager.canvas.draw_line(Point::new(505, 365), Point::new(800, 365));
+        sdl2_manager.canvas.draw_line(Point::new(505, 400), Point::new(800, 400));
+        sdl2_manager.canvas.draw_line(Point::new(505, 435), Point::new(800, 435));
+        sdl2_manager.canvas.draw_line(Point::new(505, 470), Point::new(800, 470));
+        sdl2_manager.canvas.draw_line(Point::new(505, 295), Point::new(505, 400)); // stop
+
         let _image_context = sdl2::image::init(InitFlag::PNG).unwrap();
 
         // Create a texture from the vehicle PNG
@@ -78,7 +123,7 @@ fn main() {
         let _ = match sdl2_manager.canvas.copy(
             &vehicle_texture,
             None,
-            Some(Rect::new(100, 100, 24, 24)),
+            Some(Rect::new(100, 402, 30, 30)),
         ) {
             Ok(_) => {}
             Err(e) => eprintln!("Could not copy texture to canvas: {}", e),
