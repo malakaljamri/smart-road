ad3030dbe865d7b44781f5eae0a125777105829a
Zahid Allaulddin <zahidalaaudin@gmail.com>
2026-01-09 16:31:50 +0300

    feat: add stop line indicators ("r", "s", "l") on the road

--- src/main.rs
+++ src/main.rs
@@ -124,14 +124,60 @@ fn main() {
             .canvas
             .draw_line(Point::new(330, 0), Point::new(330, 295))
             .unwrap();
+
+        //* write "r" on the road to indicate stop line
+        let r_surface = font.render("r").blended(Color::BLACK).unwrap();
+        let r_texture = texture_creator
+            .create_texture_from_surface(&r_surface)
+            .unwrap();
+
+        let r_target = Rect::new(305, 270, r_surface.width(), r_surface.height());
+
+        sdl2_manager
+            .canvas
+            .copy(&r_texture, None, Some(r_target))
+            .unwrap();
+
+        //* end of "r" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(365, 0), Point::new(365, 295))
             .unwrap();
+
+        //* write "s" on the road to indicate stop line
+        let s_surface = font.render("s").blended(Color::BLACK).unwrap();
+        let s_texture = texture_creator
+            .create_texture_from_surface(&s_surface)
+            .unwrap();
+
+        let s_target = Rect::new(340, 270, s_surface.width(), s_surface.height());
+        sdl2_manager
+            .canvas
+            .copy(&s_texture, None, Some(s_target))
+            .unwrap();
+
+        //* end of "s" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(400, 0), Point::new(400, 295))
             .unwrap();
+
+        //* write "l" on the road to indicate stop line
+        let l_surface = font.render("l").blended(Color::BLACK).unwrap();
+        let l_texture = texture_creator
+            .create_texture_from_surface(&l_surface)
+            .unwrap();
+        let l_target = Rect::new(380, 270, l_surface.width(), l_surface.height());
+
+        sdl2_manager
+            .canvas
+            .copy(&l_texture, None, Some(l_target))
+            .unwrap();
+
+        //* end of "l" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(435, 0), Point::new(435, 295))
@@ -162,10 +208,49 @@ fn main() {
             .canvas
             .draw_line(Point::new(435, 505), Point::new(435, 800))
             .unwrap();
+
+        //* write "r" on the road to indicate stop line
+        let r_surface = font.render("r").blended(Color::BLACK).unwrap();
+        let r_texture = texture_creator
+            .create_texture_from_surface(&r_surface)
+            .unwrap();
+        let r_target = Rect::new(485, 505, r_surface.width(), r_surface.height());
+        sdl2_manager
+            .canvas
+            .copy(&r_texture, None, Some(r_target))
+            .unwrap();
+
+        //* write "s" on the road to indicate stop line
+        let s_surface = font.render("s").blended(Color::BLACK).unwrap();
+        let s_texture = texture_creator
+            .create_texture_from_surface(&s_surface)
+            .unwrap();
+        let s_target = Rect::new(448, 505, s_surface.width(), s_surface.height());
+        sdl2_manager
+            .canvas
+            .copy(&s_texture, None, Some(s_target))
+            .unwrap();
+
+        //* end of "s" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(470, 505), Point::new(470, 800))
             .unwrap();
+
+        //* write "l" on the road to indicate stop line
+        let l_surface = font.render("l").blended(Color::BLACK).unwrap();
+        let l_texture = texture_creator
+            .create_texture_from_surface(&l_surface)
+            .unwrap();
+        let l_target = Rect::new(415, 505, l_surface.width(), l_surface.height());
+
+        sdl2_manager
+            .canvas
+            .copy(&l_texture, None, Some(l_target))
+            .unwrap();
+        //* end of "l" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(400, 505), Point::new(505, 505)) // stop
@@ -188,10 +273,50 @@ fn main() {
             .canvas
             .draw_line(Point::new(0, 435), Point::new(295, 435))
             .unwrap();
+
+        //* write "l" on the road to indicate stop line
+        let l_surface = font.render("l").blended(Color::BLACK).unwrap();
+        let l_texture = texture_creator
+            .create_texture_from_surface(&l_surface)
+            .unwrap();
+        let l_target = Rect::new(280, 403, l_surface.width(), l_surface.height());
+        sdl2_manager
+            .canvas
+            .copy_ex(&l_texture, None, Some(l_target), 90.0, None, false, false)
+            .unwrap();
+        //* end of "l" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(0, 470), Point::new(295, 470))
             .unwrap();
+
+        //* write "s" on the road to indicate stop line
+        let s_surface = font.render("s").blended(Color::BLACK).unwrap();
+        let s_texture = texture_creator
+            .create_texture_from_surface(&s_surface)
+            .unwrap();
+        let s_target = Rect::new(275, 438, s_surface.width(), s_surface.height());
+
+        sdl2_manager
+            .canvas
+            .copy_ex(&s_texture, None, Some(s_target), 90.0, None, false, false)
+            .unwrap();
+        //* end of "s" drawing
+
+        //* write "r" on the road to indicate stop line
+        let r_surface = font.render("r").blended(Color::BLACK).unwrap();
+        let r_texture = texture_creator
+            .create_texture_from_surface(&r_surface)
+            .unwrap();
+        let r_target = Rect::new(275, 473, r_surface.width(), r_surface.height());
+
+        sdl2_manager
+            .canvas
+            .copy_ex(&r_texture, None, Some(r_target), 90.0, None, false, false)
+            .unwrap();
+        //* end of "r" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(295, 400), Point::new(295, 505)) // stop
@@ -202,18 +327,59 @@ fn main() {
             .canvas
             .draw_line(Point::new(505, 330), Point::new(800, 330))
             .unwrap();
+
+        //* write "r" on the road to indicate stop line
+        let r_surface = font.render("r").blended(Color::BLACK).unwrap();
+        let r_texture = texture_creator
+            .create_texture_from_surface(&r_surface)
+            .unwrap();
+        let r_target = Rect::new(510, 300, r_surface.width(), r_surface.height());
+
+        sdl2_manager
+            .canvas
+            .copy_ex(&r_texture, None, Some(r_target), 270.0, None, false, false)
+            .unwrap();
+        //* end of "r" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(505, 365), Point::new(800, 365))
             .unwrap();
+
+        //* write "s" on the road to indicate stop line
+        let s_surface = font.render("s").blended(Color::BLACK).unwrap();
+        let s_texture = texture_creator
+            .create_texture_from_surface(&s_surface)
+            .unwrap();
+        let s_target = Rect::new(510, 335, s_surface.width(), s_surface.height());
+        sdl2_manager
+            .canvas
+            .copy_ex(&s_texture, None, Some(s_target), 270.0, None, false, false)
+            .unwrap();
+        //* end of "s" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(505, 400), Point::new(800, 400))
             .unwrap();
+
+        //* write "l" on the road to indicate stop line
+        let l_surface = font.render("l").blended(Color::BLACK).unwrap();
+        let l_texture = texture_creator
+            .create_texture_from_surface(&l_surface)
+            .unwrap();
+        let l_target = Rect::new(515, 370, l_surface.width(), l_surface.height());
+
+        sdl2_manager
+            .canvas
+            .copy_ex(&l_texture, None, Some(l_target), 270.0, None, false, false)
+            .unwrap();
+        //* end of "l" drawing
+
         sdl2_manager
             .canvas
             .draw_line(Point::new(505, 435), Point::new(800, 435))
-            .unwrap();
+            .unwrap(); // stop
         sdl2_manager
             .canvas
             .draw_line(Point::new(505, 470), Point::new(800, 470))
