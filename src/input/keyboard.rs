0ef04fdb9dfd632b30282ccb8f64de4130e13335
moali <mohammedali01071@gmail.com>
2026-01-13 15:28:03 +0300

    refactor: move car spawner from main to keyboard mod

--- src/input/keyboard.rs
+++ src/input/keyboard.rs
@@ -1,4 +1,5 @@
-// use crate::types::Direction;
+use crate::{intersection::Lane, render::Vehicle, types::Direction};
+use rand::Rng;
 use sdl2::keyboard::Keycode;
 
 pub struct InputHandler {
@@ -40,6 +41,66 @@ impl InputHandler {
         self.spawn_east = false;
         self.spawn_west = false;
         self.toggle_random = false;
+    }
+
+    pub fn spawn_cars(&mut self, vehicles: &mut Vec<Vehicle>) {
+        let mut rng = rand::thread_rng();
+
+        // south to north
+        if self.spawn_south {
+            let (random_dir, x) = match rng.gen_range(0..3) {
+                0 => (Direction::East, 472.5),
+                1 => (Direction::West, 402.5),
+                _ => (Direction::North, 437.5),
+            };
+
+            let lane = Lane::set(Direction::South, random_dir);
+            let vehicle = Vehicle::new(vehicles.len(), x, 800.0, lane);
+            println!("vehicle: {:?}", vehicle);
+            vehicles.push(vehicle);
+        }
+
+        // north to south
+        if self.spawn_north {
+            let (random_dir, x) = match rng.gen_range(0..3) {
+                0 => (Direction::East, 365.0),
+                1 => (Direction::West, 295.0),
+                _ => (Direction::South, 330.0),
+            };
+
+            let lane = Lane::set(Direction::North, random_dir);
+            let vehicle = Vehicle::new(vehicles.len(), x, 0.0, lane);
+            println!("vehicle: {:?}", vehicle);
+            vehicles.push(vehicle);
+        }
+
+        // east to west
+        if self.spawn_east {
+            let (random_dir, y) = match rng.gen_range(0..3) {
+                0 => (Direction::North, 295.0),
+                1 => (Direction::South, 365.0),
+                _ => (Direction::West, 330.0),
+            };
+
+            let lane = Lane::set(Direction::East, random_dir);
+            let vehicle = Vehicle::new(vehicles.len(), 800.0, y, lane);
+            println!("vehicle: {:?}", vehicle);
+            vehicles.push(vehicle);
+        }
+
+        // west to east
+        if self.spawn_west {
+            let (random_dir, y) = match rng.gen_range(0..3) {
+                0 => (Direction::North, 402.5),
+                1 => (Direction::South, 472.5),
+                _ => (Direction::East, 437.5),
+            };
+
+            let lane = Lane::set(Direction::West, random_dir);
+            let vehicle = Vehicle::new(vehicles.len(), 0.0, y, lane);
+            println!("vehicle: {:?}", vehicle);
+            vehicles.push(vehicle);
+        }
     }
 
     //? Might be useful later
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,10 @@ mod types;
 
 use crate::{
     input::InputHandler,
-    render::{Statistics, TextureCache, draw_lanes},
-    types::Direction,
+    render::{Statistics, TextureCache, draw_intersection},
 };
-use intersection::Lane;
-use rand::Rng;
 use render::{Sdl2Manager, Vehicle};
-use sdl2::{image::InitFlag, pixels::Color, rect::Rect, render::TextureCreator};
+use sdl2::{image::InitFlag, rect::Rect, render::TextureCreator};
 
 /*
 lane width = 35px
@@ -48,7 +45,6 @@ fn main() {
         sdl2_manager.canvas.texture_creator();
     let texture_cache = TextureCache::new(&texture_creator);
 
-    let mut rng = rand::thread_rng();
     let mut showing_stats = false;
 
     'running: loop {
@@ -93,100 +89,9 @@ fn main() {
             continue;
         }
 
-        // south to north
-        if input.spawn_south {
-            let (random_dir, x) = match rng.gen_range(0..3) {
-                0 => (Direction::East, 472.5),
-                1 => (Direction::West, 402.5),
-                _ => (Direction::North, 437.5),
-            };
+        draw_intersection(&mut sdl2_manager, &font, &texture_creator);
 
-            let lane = Lane::set(Direction::South, random_dir);
-            let vehicle = Vehicle::new(vehicles.len(), x, 800.0, lane);
-            println!("vehicle: {:?}", vehicle);
-            vehicles.push(vehicle);
-        }
-
-        // north to south
-        if input.spawn_north {
-            let (random_dir, x) = match rng.gen_range(0..3) {
-                0 => (Direction::East, 365.0),
-                1 => (Direction::West, 295.0),
-                _ => (Direction::South, 330.0),
-            };
-
-            let lane = Lane::set(Direction::North, random_dir);
-            let vehicle = Vehicle::new(vehicles.len(), x, 0.0, lane);
-            println!("vehicle: {:?}", vehicle);
-            vehicles.push(vehicle);
-        }
-
-        // east to west
-        if input.spawn_east {
-            let (random_dir, y) = match rng.gen_range(0..3) {
-                0 => (Direction::North, 295.0),
-                1 => (Direction::South, 365.0),
-                _ => (Direction::West, 330.0),
-            };
-
-            let lane = Lane::set(Direction::East, random_dir);
-            let vehicle = Vehicle::new(vehicles.len(), 800.0, y, lane);
-            println!("vehicle: {:?}", vehicle);
-            vehicles.push(vehicle);
-        }
-
-        // west to east
-        if input.spawn_west {
-            let (random_dir, y) = match rng.gen_range(0..3) {
-                0 => (Direction::North, 402.5),
-                1 => (Direction::South, 472.5),
-                _ => (Direction::East, 437.5),
-            };
-
-            let lane = Lane::set(Direction::West, random_dir);
-            let vehicle = Vehicle::new(vehicles.len(), 0.0, y, lane);
-            println!("vehicle: {:?}", vehicle);
-            vehicles.push(vehicle);
-        }
-
-        // Set the draw color to yellow and clear the window each frame
-        sdl2_manager
-            .canvas
-            .set_draw_color(Color::RGB(255, 235, 59));
-        sdl2_manager.canvas.clear();
-
-        // corner rects
-        sdl2_manager.canvas.set_draw_color(Color::RGB(0, 0, 0));
-        let top_left = Rect::new(0, 0, 295, 295);
-        let top_right = Rect::new(505, 0, 295, 295);
-        let bottom_left = Rect::new(0, 505, 295, 295);
-        let bottom_right = Rect::new(505, 505, 295, 295);
-
-        // draw corner rects
-        sdl2_manager.canvas.fill_rect(top_left).unwrap();
-        sdl2_manager.canvas.fill_rect(top_right).unwrap();
-        sdl2_manager.canvas.fill_rect(bottom_left).unwrap();
-        sdl2_manager.canvas.fill_rect(bottom_right).unwrap();
-
-        //* Font and text */
-        let text_surface = font.render("Smart Road").blended(Color::BLACK).unwrap();
-        let texture_creator = sdl2_manager.canvas.texture_creator();
-        let text_texture = texture_creator
-            .create_texture_from_surface(&text_surface)
-            .unwrap();
-
-        let target = Rect::new(
-            800 / 2 - (text_surface.width() as i32) / 2,
-            400 - (text_surface.height() as i32) / 2,
-            text_surface.width(),
-            text_surface.height(),
-        );
-        sdl2_manager
-            .canvas
-            .copy(&text_texture, None, Some(target))
-            .unwrap();
-
-        draw_lanes(&mut sdl2_manager, &font, &texture_creator);
+        input.spawn_cars(&mut vehicles);
 
         // update vehicles
         for vehicle in &mut vehicles {
